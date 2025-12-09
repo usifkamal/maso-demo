53c08d5202e7eec83a7e47670770c9309f38d3ca
Youssef Kamal <youssef575757@gmail.com>
2025-12-10 00:51:05 +0200

    Fix IconMenu export: Update function signature to match other icons

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -577,9 +577,16 @@ function IconPalette({ className, ...props }: React.ComponentProps<'svg'>) {
   )
 }
 
-function IconMenu({ className }: { className?: string }) {
+function IconMenu({ className, ...props }: React.ComponentProps<'svg'>) {
   return (
-    <svg className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className={cn('h-4 w-4', className)}
+      {...props}
+    >
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
     </svg>
   )
