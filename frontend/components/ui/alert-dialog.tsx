ab7358a9c06d4fe9d61c81fd7ad6d9d51aed91a2
Youssef Kamal <youssef575757@gmail.com>
2025-12-10 22:42:02 +0200

    Fix AlertDialogPortal - remove unsupported className prop

--- frontend/components/ui/alert-dialog.tsx
+++ frontend/components/ui/alert-dialog.tsx
@@ -10,18 +10,7 @@ const AlertDialog = AlertDialogPrimitive.Root
 
 const AlertDialogTrigger = AlertDialogPrimitive.Trigger
 
-const AlertDialogPortal = ({
-  className,
-  children,
-  ...props
-}: AlertDialogPrimitive.AlertDialogPortalProps) => (
-  <AlertDialogPrimitive.Portal className={cn(className)} {...props}>
-    <div className="fixed inset-0 z-50 flex items-end justify-center sm:items-center">
-      {children}
-    </div>
-  </AlertDialogPrimitive.Portal>
-)
-AlertDialogPortal.displayName = AlertDialogPrimitive.Portal.displayName
+const AlertDialogPortal = AlertDialogPrimitive.Portal
 
 const AlertDialogOverlay = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Overlay>,
