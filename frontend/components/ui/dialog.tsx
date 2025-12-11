ac8552e2f4195c4a1412cca493ad8bad41788e29
Youssef Kamal <youssef575757@gmail.com>
2025-12-11 02:00:53 +0200

    Fix DialogPortal component - remove unsupported className prop

--- frontend/components/ui/dialog.tsx
+++ frontend/components/ui/dialog.tsx
@@ -10,18 +10,7 @@ const Dialog = DialogPrimitive.Root
 
 const DialogTrigger = DialogPrimitive.Trigger
 
-const DialogPortal = ({
-  className,
-  children,
-  ...props
-}: DialogPrimitive.DialogPortalProps) => (
-  <DialogPrimitive.Portal className={cn(className)} {...props}>
-    <div className="fixed inset-0 z-50 flex items-start justify-center sm:items-center">
-      {children}
-    </div>
-  </DialogPrimitive.Portal>
-)
-DialogPortal.displayName = DialogPrimitive.Portal.displayName
+const DialogPortal = DialogPrimitive.Portal
 
 const DialogOverlay = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Overlay>,
