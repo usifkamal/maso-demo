94814c7eab2bdde2ed464ea4b57e1a248457bc80
Youssef Kamal <youssef575757@gmail.com>
2025-12-11 02:04:35 +0200

    Fix SheetPortal component - remove unsupported className prop

--- frontend/components/ui/sheet.tsx
+++ frontend/components/ui/sheet.tsx
@@ -12,19 +12,7 @@ const SheetTrigger = SheetPrimitive.Trigger
 
 const SheetClose = SheetPrimitive.Close
 
-const SheetPortal = ({
-  className,
-  children,
-  ...props
-}: SheetPrimitive.DialogPortalProps) => (
-  <SheetPrimitive.Portal
-    className={cn('fixed inset-0 z-50 flex', className)}
-    {...props}
-  >
-    {children}
-  </SheetPrimitive.Portal>
-)
-SheetPortal.displayName = SheetPrimitive.Portal.displayName
+const SheetPortal = SheetPrimitive.Portal
 
 const SheetOverlay = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Overlay>,
